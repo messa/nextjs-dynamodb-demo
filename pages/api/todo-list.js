1a02d66fe70bcd4bb0807cc0f4b76bc9b688a5d5
Petr Messner <petr.messner@gmail.com>
2021-09-03 14:25:47 +0200

    add try-catch in pages/api/todo-list.js

--- pages/api/todo-list.js
+++ pages/api/todo-list.js
@@ -2,19 +2,33 @@ import { getSession } from "next-auth/client"
 import { listTodoItems } from "../../lib/model"
 
 async function handler(req, res) {
-  const session = await getSession({ req })
-  if (!session) {
-    return res.status(403).json({ error: { code: 'not_authenticated' } })
-  }
-  const userEmail = session.user.email
-  if (!userEmail) {
-    throw new Error('user e-mail missing')
-  }
-  const todoItems = await listTodoItems(userEmail)
+  try {
 
-  res.status(200).json({
-    todoItems,
-  })
+    const session = await getSession({ req })
+    if (!session) {
+      return res.status(403).json({ error: { code: 'not_authenticated' } })
+    }
+
+    const userEmail = session.user.email
+    if (!userEmail) {
+      throw new Error('user e-mail missing')
+    }
+
+    const todoItems = await listTodoItems(userEmail)
+
+    return res.status(200).json({
+      todoItems,
+    })
+
+  } catch (err) {
+    console.error('Todo list failed:', err)
+    return res.status(500).json({
+      error: {
+        code: 'internal_error',
+        message: err.toString(),
+      }
+    })
+  }
 }
 
 export default handler
